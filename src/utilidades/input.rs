43ee819040e0b0e75931eb49ef8fca27dc3adbd8
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-04 18:28:09 -0300

    feat: :sparkles: Fn obtener_texto, para obtener input del usuario

--- src/utilidades/input.rs
+++ src/utilidades/input.rs
@@ -2,13 +2,7 @@ use std::io;
 
 pub fn obtener_opcion_valida(primera_opcion: u8, ultima_opcion: u8) -> u8 {
     return loop {
-        println!("Opcion [{primera_opcion}-{ultima_opcion}]: ");
-
-        let mut op_usuario = String::new();
-
-        io::stdin()
-            .read_line(&mut op_usuario)
-            .expect("Fallo al leer la linea");
+        let op_usuario = obtener_texto(format!("Opcion [{primera_opcion}-{ultima_opcion}]: "));
 
         if let Ok(num) = op_usuario.trim().parse::<u8>() {
             if primera_opcion <= num && num <= ultima_opcion {
@@ -19,4 +13,16 @@ pub fn obtener_opcion_valida(primera_opcion: u8, ultima_opcion: u8) -> u8 {
         println!("Opcion no valida!!");
         println!("Selecione dentro del rango dado\n");
     };
+}
+
+pub fn obtener_texto(texto: String) -> String {
+    println!("{texto}");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Fallo al leer la linea");
+
+    input.trim().to_string()
 }
