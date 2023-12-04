451eb70ba3aa6fc1a5265d69e784cec8f236d5f1
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-04 18:01:59 -0300

    feat: :sparkles: Creando funcion contacto_filtrar_por_propiedad, la cual filtra los contactos segun una propiedad y valor que se le pase

--- src/contacto.rs
+++ src/contacto.rs
@@ -1,8 +1,17 @@
+#[derive(Clone)]
 pub struct Contacto {
     nombre: String,
     apellido: String,
     telefono: String,
     email: String,
+}
+
+#[derive(Debug)]
+pub enum ContactoPropiedad {
+    Nombre,
+    Apellido,
+    Telefono,
+    Email,
 }
 
 // Fn estaticas
@@ -34,4 +43,51 @@ pub fn contacto_imprimir_vector(contactos: &Vec<Contacto>) {
         cont.imprimir()
     }
     println!("-------------------------------");
+}
+
+pub fn contacto_filtrar_por_propiedad(
+    contactos: &Vec<Contacto>,
+    propiedad: ContactoPropiedad,
+    valor: String,
+) -> Vec<Contacto> {
+    contactos
+        .iter()
+        .filter_map(|contacto| {
+            match propiedad {
+                ContactoPropiedad::Nombre
+                    if contacto
+                        .nombre
+                        .to_lowercase()
+                        .contains(valor.to_lowercase().as_str()) =>
+                {
+                    Some(contacto.clone())
+                }
+                ContactoPropiedad::Apellido
+                    if contacto
+                        .apellido
+                        .to_lowercase()
+                        .contains(valor.to_lowercase().as_str()) =>
+                {
+                    Some(contacto.clone())
+                }
+                ContactoPropiedad::Telefono
+                    if contacto
+                        .telefono
+                        .to_lowercase()
+                        .contains(valor.to_lowercase().as_str()) =>
+                {
+                    Some(contacto.clone())
+                }
+                ContactoPropiedad::Email
+                    if contacto
+                        .email
+                        .to_lowercase()
+                        .contains(valor.to_lowercase().as_str()) =>
+                {
+                    Some(contacto.clone())
+                }
+                _ => None, // Manejar otros casos aquí si es necesario
+            }
+        })
+        .collect()
 }
