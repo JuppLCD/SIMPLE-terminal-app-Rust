8c3c80033e3325694b398715f984d228840e0d42
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-04 13:49:09 -0300

    feat: :sparkles: La opcion mostrar todos ahora funciona con contactos en memoria

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,32 @@ pub mod utilidades;
 use crate::imprimir::{imprimir_msg_salir_agenda, imprimir_opciones_agenda};
 pub mod imprimir;
 
+use crate::contacto::Contacto;
+pub mod contacto;
+
 fn main() {
+    // Contactos de prueba
+    let contacto1 = Contacto::nuevo(
+        "Lorenzo".to_string(),
+        "Canovas".to_string(),
+        "846857".to_string(),
+        "lorenzo@gmail.com".to_string(),
+    );
+    let contacto2 = Contacto::nuevo(
+        "Pepe".to_string(),
+        "Gonsalez".to_string(),
+        "65436".to_string(),
+        "pepe@gmail.com".to_string(),
+    );
+    let contactos = vec![contacto1, contacto2];
+
     loop {
         imprimir_opciones_agenda();
 
         let op = obtener_opcion_valida(0, 5);
 
         match AgendaContactosOpciones::new(op) {
-            AgendaContactosOpciones::MostrarTodos => mostrar_todos_contactos(),
+            AgendaContactosOpciones::MostrarTodos => mostrar_todos_contactos(&contactos),
             AgendaContactosOpciones::Buscar => buscar_contacto(),
             AgendaContactosOpciones::Agregar => agregar_contacto(),
             AgendaContactosOpciones::Editar => editar_contacto(),
--- src/opciones_agenda_contacto/mostrar_todos.rs
+++ src/opciones_agenda_contacto/mostrar_todos.rs
@@ -1,7 +1,9 @@
+use crate::contacto::{contacto_imprimir_vector, Contacto};
 use crate::imprimir::imprimir_cabecera_mostrar_todos_contactos;
 use crate::utilidades::terminal::pausar_terminal;
 
-pub fn mostrar_todos_contactos() {
+pub fn mostrar_todos_contactos(contactos: &Vec<Contacto>) {
     imprimir_cabecera_mostrar_todos_contactos();
+    contacto_imprimir_vector(contactos);
     pausar_terminal();
 }
